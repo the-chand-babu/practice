a88fb6e776679c65b697a1d8406312b44a091f38
chand-Pc <jafri.nbj@gmail.com>
2023-01-14 16:15:04 +0530

    changes

---
+++ day-8/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const {student} = require('./router/student');
+const {teacher} =require('./router/teacher');
+const cors = require('cors');
+
+const app = express();
+app.use(cors())
+app.use(express.json());
+const watchMan=(req,res,next)=>{
+    console.log(req.url);
+    console.log(req.method);
+    
+    next()
+    }
+    app.use(watchMan)
+
+app.use('/student',student);
+app.use('/teacher',teacher);
+
+
+
+
+
+app.listen(3001,(err)=>{
+    console.log("port is lisning");
+})
+
+console.log("wroking")
